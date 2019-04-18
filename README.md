032128d445e4307272f112eb20995d96ed4b7307
ilaria <ilaria@ultrafab.it>
2019-04-18 15:33:00 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+
+# 	SplitString - Activity
---
+++ activity.go
@@ -0,0 +1,33 @@
+package SplitString
+
+import (
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"github.com/TIBCOSoftware/flogo-lib/logger"
+	"strings"
+)
+var log = logger.GetLogger("activity-helloworld")
+// MyActivity is a stub for your Activity implementation
+type MyActivity struct {
+	metadata *activity.Metadata
+}
+
+// NewActivity creates a new activity
+func NewActivity(metadata *activity.Metadata) activity.Activity {
+	return &MyActivity{metadata: metadata}
+}
+
+// Metadata implements activity.Activity.Metadata
+func (a *MyActivity) Metadata() *activity.Metadata {
+	return a.metadata
+}
+
+// Eval implements activity.Activity.Eval
+func (a *MyActivity) Eval(context activity.Context) (done bool, err error)  {
+
+	input_string := context.GetInput("input_string").(string)
+	split_char := context.GetInput("split_char").(string)
+	log.Infof("String to parse: [%s] with char: [%s]", input_string, split_char)
+	result := strings.Split(input_string, split_char)
+	context.SetOutput("result", result)
+	return true, nil
+}
